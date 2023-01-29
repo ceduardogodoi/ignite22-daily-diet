08e18b50b404b4986cc5a05894d3eddd971ff4d7
Beatriz Ramos <carloseduardoalvesgodoi@hotmail.com>
2023-01-29 11:24:35 -0300

    Adiciona o ThemeProvider

--- App.tsx
+++ App.tsx
@@ -1,6 +1,9 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, View } from 'react-native'
+import { ThemeProvider } from 'styled-components'
+import styled from 'styled-components/native'
 import { Nunito_400Regular, Nunito_700Bold, useFonts } from '@expo-google-fonts/nunito'
 import { StatusBar } from 'expo-status-bar'
+import { theme } from './src/theme'
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -9,12 +12,19 @@ export default function App() {
   })
 
   return fontsLoaded && (
-    <View style={styles.container}>
-      <Text style={{ fontFamily: 'Nunito_700Bold' }}>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider theme={theme}>
+      <View style={styles.container}>
+        <Text>Open up App.tsx to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </ThemeProvider>
   )
 }
+
+const Text = styled.Text`
+  color: ${props => props.theme.colors['green-dark']};
+  font-family: ${props => props.theme.fonts.bold};
+`
 
 const styles = StyleSheet.create({
   container: {
