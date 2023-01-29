1022405cf4cc0b91458bc720183e19da5a30e2ec
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2023-01-29 11:40:12 -0300

    Adiciona path aliases

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import { ThemeProvider } from 'styled-components'
 import styled from 'styled-components/native'
 import { Nunito_400Regular, Nunito_700Bold, useFonts } from '@expo-google-fonts/nunito'
 import { StatusBar } from 'expo-status-bar'
-import { theme } from './src/theme'
+import { theme } from '@theme'
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -14,16 +14,15 @@ export default function App() {
   return fontsLoaded && (
     <ThemeProvider theme={theme}>
       <View style={styles.container}>
-        <Text>Open up App.tsx to start working on your app!</Text>
         <StatusBar style="auto" />
+        <Text>Test 123</Text>
       </View>
     </ThemeProvider>
   )
 }
 
 const Text = styled.Text`
-  color: ${props => props.theme.colors['green-dark']};
-  font-family: ${props => props.theme.fonts.bold};
+  font-family: ${props => props.theme.fonts.bold}
 `
 
 const styles = StyleSheet.create({
