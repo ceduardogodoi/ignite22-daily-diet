668b87d0341318ab0bbea03a7fc9d540adb21aa0
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2023-03-14 23:36:21 -0300

    Finaliza estilização da página de estatísticas

--- src/screens/Statistics/index.tsx
+++ src/screens/Statistics/index.tsx
@@ -1,5 +1,4 @@
-import { Image, StatusBar } from 'react-native';
-import { useTheme } from 'styled-components/native';
+import { FlatList, TouchableOpacity } from 'react-native';
 
 import {
   Header,
@@ -8,22 +7,46 @@ import {
   Heading,
   Container,
   MainContent,
-  BackButton,
+  Box,
+  BoxTitle,
+  BoxDescription,
+  StatusBar,
+  LeftArrowButton,
 } from './styles';
 
-import leftArrow from '@assets/left-arrow.png'
+import type { BoxStatus } from './styles'
+
+type Data = {
+  id: string,
+  amount: number,
+  title: string,
+  status: BoxStatus,
+}[]
+
+const data: Data = [
+  {
+    id: '1',
+    amount: 99,
+    title: 'refeições dentro da dieta',
+    status: 'success',
+  },
+  {
+    id: '2',
+    amount: 10,
+    title: 'refeições dentro fora da dieta',
+    status: 'fail',
+  },
+];
 
 export function Statistics() {
-  const theme = useTheme()
-
   return (
     <Container>
-      <StatusBar backgroundColor={theme.colors['green-light']} barStyle="dark-content" />
+      <StatusBar barStyle="dark-content" />
 
       <Header>
-        <BackButton>
-          <Image source={leftArrow} />
-        </BackButton>
+        <TouchableOpacity>
+          <LeftArrowButton />
+        </TouchableOpacity>
 
         <Percentage>
           90,86%
@@ -35,6 +58,40 @@ export function Statistics() {
 
       <MainContent>
         <Heading>Estatísticas gerais</Heading>
+
+        <Box>
+          <BoxTitle>22</BoxTitle>
+
+          <BoxDescription>
+            melhor sequência de pratos dentro da dieta
+          </BoxDescription>
+        </Box>
+
+        <Box>
+          <BoxTitle>109</BoxTitle>
+
+          <BoxDescription>
+            melhor sequência de pratos dentro da dieta
+          </BoxDescription>
+        </Box>
+
+        <FlatList
+          data={data}
+          keyExtractor={item => item.id}
+          renderItem={({ item }) => (
+            <Box fullWidth={false} status={item.status}>
+              <BoxTitle>{item.amount}</BoxTitle>
+
+              <BoxDescription>
+                {item.title}
+              </BoxDescription>
+            </Box>
+          )}
+          contentContainerStyle={{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+          }}
+        />
       </MainContent>
     </Container>
   )
---
+++ src/screens/Statistics/styles.tsx
@@ -0,0 +1,138 @@
+import { SvgProps } from 'react-native-svg';
+import { SafeAreaView } from 'react-native-safe-area-context'
+import styled, { css } from 'styled-components/native'
+
+import { theme } from '@theme'
+
+import LeftArrow from '@assets/left-arrow.svg'
+
+const boxStatus = {
+  neutral: theme.colors['gray-600'],
+  success: theme.colors['green-light'],
+  fail: theme.colors['red-light'],
+} as const;
+
+export type BoxStatus = keyof typeof boxStatus;
+
+type ContainerProps = {
+  status?: BoxStatus
+}
+
+export const Container = styled(SafeAreaView) <ContainerProps>`
+  flex: 1;
+
+  ${({ status = 'neutral' }) => css`
+    background-color: ${boxStatus[status]};
+  `};
+`
+
+export const Header = styled.View<ContainerProps>`
+  padding: 34px 24px;
+
+  ${({ status = 'neutral' }) => css`
+    background-color: ${boxStatus[status]};
+  `};
+`
+
+export const BackButton = styled.TouchableOpacity``
+
+export const Percentage = styled.Text`
+  line-height: 42px;
+  text-align: center;
+
+  ${({ theme }) => css`
+    font-family: ${theme.fonts.bold};
+    font-size: ${theme.fontSizes.xxl}px;
+    color: ${theme.colors['gray-100']};
+  `};
+`
+
+export const Description = styled.Text`
+  line-height: 18px;
+  text-align: center;
+
+  ${({ theme }) => css`
+    font-family: ${theme.fonts.regular};
+    font-size: ${theme.fontSizes.sm}px;
+    color: ${theme.colors['gray-200']};
+  `};
+`
+
+export const MainContent = styled.View`
+  flex: 1;
+  border-radius: 20px;
+  padding: 34px 24px 0;
+
+  ${({ theme }) => css`
+    background-color: ${theme.colors['gray-700']};
+  `};
+`
+
+export const Heading = styled.Text`
+  line-height: 18px;
+  text-align: center;
+  margin-bottom: 23px;
+
+  ${({ theme }) => css`
+    font-family: ${theme.fonts.bold};
+    font-size: ${theme.fontSizes.sm}px;
+    color: ${theme.colors['gray-100']};
+  `};
+`
+
+type BoxProps = {
+  status?: BoxStatus
+  fullWidth?: boolean
+}
+
+export const Box = styled.View<BoxProps>`
+  padding: 16px;
+  border-radius: 8px;
+  margin-bottom: 12px;
+
+  ${({ status = 'neutral', fullWidth = true }) => css`
+    width: ${fullWidth ? '100%' : '167px'};
+    background-color: ${boxStatus[status]};
+  `};
+`
+
+export const BoxTitle = styled.Text`
+  line-height: 31px;
+  text-align: center;
+
+  ${({ theme }) => css`
+    font-family: ${theme.fonts.bold};
+    font-size: ${theme.fontSizes.xl}px;
+    color: ${theme.colors['gray-100']};
+  `};
+`
+
+export const BoxDescription = styled.Text`
+  line-height: 18px;
+  text-align: center;
+  margin-top: 8px;
+
+  ${({ theme }) => css`
+    font-family: ${theme.fonts.regular};
+    font-size: ${theme.fontSizes.sm}px;
+    color: ${theme.colors['gray-200']};
+  `};
+`
+
+const arrowStatus = {
+  neutral: 'gray',
+  success: 'green',
+  fail: 'red',
+} as const;
+
+type LeftArrowButtonProps = SvgProps & {
+  status?: keyof typeof arrowStatus
+};
+
+export function LeftArrowButton({ status = 'neutral', ...rest }: LeftArrowButtonProps) {
+  return <LeftArrow {...rest} stroke={arrowStatus[status]} />
+}
+
+export const StatusBar = styled.StatusBar.attrs<ContainerProps>(({ status = 'neutral' }) => ({
+  backgroundColor: boxStatus[status],
+}))``;
