6f31d27b09d4969cda7c76e8bb00471053cccf6e
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2023-04-26 09:38:48 -0300

    Adiciona máscara para Hora

--- src/utils/masks.ts
+++ src/utils/masks.ts
@@ -15,7 +15,10 @@ export function toDate(value?: string) {
 }
 
 export function toTime(value?: string) {
-  value = value?.replace(/\D/, '')
-  value = value?.replace(/(\d{2})(\d{2})/, '$1:$2')
+  if (!value) return value
+  value = value.replace(/\D/, '')
+  if (value.length > 2) {
+    value = value.replace(/(\d{2})(\d+)/, '$1:$2')
+  }
   return value
 }
